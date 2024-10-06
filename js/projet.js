99fa3c7a735b6265123f2f2539111e9121069114
Diamoutene1 <tidianediamoutene100@gmail.com>
2024-10-06 18:01:49 +0200

    Mise à jour du formulaire de contact et ajout de styles

--- js/projet.js
+++ js/projet.js
@@ -11,3 +11,20 @@ projects.forEach(project => {
         project.style.transform = 'scale(1)';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.details-button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const details = this.nextElementSibling; // Récupère le div qui suit le bouton
+            if (details.style.display === 'none' || details.style.display === '') {
+                details.style.display = 'block'; // Affiche les détails
+                this.textContent = 'Masquer les détails'; // Change le texte du bouton
+            } else {
+                details.style.display = 'none'; // Masque les détails
+                this.textContent = 'Voir les détails'; // Restaure le texte du bouton
+            }
+        });
+    });
+});
